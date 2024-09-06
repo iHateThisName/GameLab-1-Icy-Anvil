b2c94a9a37ea3e1efe6b4285d73d703a244fb583
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2024-09-06 17:23:56 +0200

    Death spike

--- Assets/_Scripts/DeathScript.cs
+++ Assets/_Scripts/DeathScript.cs
@@ -4,30 +4,18 @@ using UnityEngine;
 
 public class DeathScript : MonoBehaviour
 {
-    public bool player;
+    //public Transform startPos;
+    public Vector3 playerStartPos;
+    public GameObject playerPref;
 
-    private Vector3 startPos;
-    private Rigidbody2D rb;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        startPos = transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            transform.position = startPos;
+            Debug.Log("Die");
+            Destroy(other.gameObject);
+            Instantiate(playerPref, playerStartPos, Quaternion.identity);
         }
     }
-    
+   
 }
